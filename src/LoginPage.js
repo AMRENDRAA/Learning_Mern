2af908e0efa48419f03242441e6ce0739a5faf74
AMRENDRA <amrendraraj.cse@gmail.com>
2025-02-20 08:45:54 +0530

    completed

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,10 +1,70 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 export const LoginPage = () => {
- console.log('test');
+//  console.log('test');
+ const [loginData,setLoginData]=useState({
+
+  username :'',
+  password :'',
+ })
+
+
+ //submit function
+
+ const handleLoginSubmit= async (e)=>{
+  e.preventDefault();
+  try {
+    const response = await axios.post('http://localhost:8000/login', loginData);
+    const { success, message } = response.data;
+
+    if (success) {
+      console.log('Login successful');
+    } else {
+      console.log(message);
+    }
+  } catch (error) {
+    console.error('Login error', error);
+  }
+};
+
+ const  handleLoginChange =(e)=>{
+
+  console.log(e);
+  const {name,value}=e.target;
+  console.log(`Updating ${name} to: ${value}`);
+  setLoginData((prevData)=>({
+    ...prevData,
+    [name]:value
+
+
+  }))
+
+
+ }
     return (
     
-    <div>LoginPage</div>
+    <div>
+    <h1> LoginPage       </h1>
+    <form onSubmit={handleLoginSubmit}>
+      <input type='text' name ='username' placeholder='User Email' required  onChange={handleLoginChange} value={loginData.username}></input>
+      <input type='password' name ='password' placeholder='password' required  onChange={handleLoginChange} value={loginData.password}></input>
+      <button type='submit'>Login </button>
+      <p> Not registered yet ?
+      
+      <Link to='/registration'>Register here</Link>
+
+
+
+      </p>
+
+    </form>
+    
+    
+    
+    
+    
+    </div>
   )
 }
 export default LoginPage;
--- src/RegistrationPage.js
+++ src/RegistrationPage.js
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export const RegistrationPage = () => {
+
+const [RegistrationData,setRegistrationData]=useState({
+    username:'',
+    password:''
+  
+  })
+
+const handleRegistrationChange=(e)=>{
+
+  const {name ,value}=e.target;
+  setRegistrationData((prevData)=>({
+
+    ...prevData,
+    [name]:value,
+
+  }))
+
+}
+const handleRegistrationSubmit=async(e)=>{
+  e.preventDefault();
+  try{
+    const response = await axios.post('http://localhost:8000/register',RegistrationData);
+    console.log(response.data );
+  }catch(error){
+    console.log(error);
+  }
+
+  setRegistrationData({
+    username:'',
+    password:'',
+  })
+
+
+
+
+}
+
   return (
-    <div>RegistrationPage</div>
+    <div>
+
+<h1> Registration Form </h1>
+    <form onSubmit={handleRegistrationSubmit}>
+    
+    <input type='text' name='username ' placeholder='username ' onchange={handleRegistrationChange} value={RegistrationData.username} required></input>
+    
+     <input type='password' name='password' placeholder='password' onchange={handleRegistrationChange} value={RegistrationData.username} required></input>
+    <button type='submit'>Register</button>
+    <p>Already Registered 
+    <Link to="/login">Login Here </Link>
+
+
+    </p>
+    
+      </form>
+
+
+
+
+
+    </div>
+    
+
   )
 }
 export default RegistrationPage;
